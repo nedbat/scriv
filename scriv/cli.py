8cadb46deb7f81e628d64bdbe481ebd49bcee0cb
Ned Batchelder <ned@nedbatchelder.com>
2019-12-30 06:02:53 -0500

    Use click-log

--- scriv/cli.py
+++ scriv/cli.py
@@ -1,9 +1,15 @@
 """Scriv command-line interface."""
 
+import logging
+
 import click
+import click_log
 
 from scriv.config import read_config
 from scriv.create import new_entry_contents, new_entry_path
+
+logger = logging.getLogger(__name__)
+click_log.basic_config(logger)
 
 
 @click.group()
@@ -12,12 +18,13 @@ def cli() -> None:
 
 
 @cli.command()
+@click_log.simple_verbosity_option(logger)
 def create() -> None:
     """
     Create a new scriv changelog entry.
     """
     config = read_config()
     file_path = new_entry_path(config)
-    click.echo("Creating {}".format(file_path))
+    logger.info("Creating {}".format(file_path))
     with open(file_path, "w") as f:
         f.write(new_entry_contents(config))
