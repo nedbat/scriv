4c223be5c9612610414138a2428b439ab46808c5
Ned Batchelder <ned@nedbatchelder.com>
2019-12-31 22:40:56 -0500

    Add some logging of shell commands

--- scriv/shell.py
+++ scriv/shell.py
@@ -1,5 +1,6 @@
 """Helpers for using subprocesses."""
 
+import logging
 import shlex
 import subprocess
 from typing import Tuple
@@ -14,5 +15,9 @@ def run_command(cmd: str) -> Tuple[bool, str]:
         str: the output of the command.
 
     """
+    logging.debug("Running command {!r}".format(cmd))
     proc = subprocess.run(shlex.split(cmd), shell=False, check=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    return proc.returncode == 0, proc.stdout.decode("utf-8")
+    output = proc.stdout.decode("utf-8")
+    logging.debug("Command exited with {} status. Output: {!r}".format(proc.returncode, output))
+
+    return proc.returncode == 0, output
