e436205eaad5b180ea0312a566e6513f81b89927
Ned Batchelder <ned@nedbatchelder.com>
2020-01-02 17:22:27 -0500

    Configuration is read from a file

--- scriv/config.py
+++ scriv/config.py
@@ -1,5 +1,9 @@
 """Scriv configuration."""
 
+import configparser
+import re
+from typing import Any, List
+
 import attr
 
 
@@ -16,8 +20,39 @@ class Config:
     insert_marker = attr.ib(type=str, default="scriv:insert-here")
 
 
+def convert_list(val: str) -> List[str]:
+    """
+    Convert a string value from a config into a list of strings.
+
+    Elements can be separated by commas or newlines.
+    """
+    vals = re.split(r"[\n,]", val)
+    vals = [v.strip() for v in vals]
+    vals = [v for v in vals if v]
+    return vals
+
+
 def read_config() -> Config:
     """
     Read the configuration to use.
+
+    Configuration will be read from .scrivrc, setup.cfg, or tox.ini.
+    The section can be named ``[scriv]`` or ``[tool.scriv]``.
+
     """
-    return Config()
+    config = Config()
+    parser = configparser.ConfigParser()
+    parser.read([".scrivrc", "setup.cfg", "tox.ini"])
+    section_names = ["scriv", "tool.scriv"]
+    section_name = next((name for name in section_names if parser.has_section(name)), None)
+    if section_name:
+        for attrdef in attr.fields(Config):
+            try:
+                val = parser[section_name][attrdef.name]  # type: Any
+            except KeyError:
+                pass
+            else:
+                if attrdef.type is list:
+                    val = convert_list(val)
+                setattr(config, attrdef.name, val)
+    return config
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,10 +1,47 @@
 """Tests of scriv/config.py"""
 
-from scriv.config import Config
+from scriv.config import read_config
+
+CONFIG1 = """\
+[scriv]
+output_file = README.md
+categories = New, Different, Gone, Bad
+"""
+
+CONFIG2 = """\
+[someotherthing]
+no_idea = what this is
+
+[tool.scriv]
+output_file = README.md
+categories =
+    New
+    Different
+    Gone
+    Bad
+
+[more stuff]
+value = 17
+"""
 
 
-def test_defaults():
-    config = Config()
+def test_defaults(temp_dir):  # pylint: disable=unused-argument
+    config = read_config()
     assert config.entry_directory == "changelog.d"
     assert config.format == "rst"
     assert config.output_file == "CHANGELOG.rst"
+    assert config.insert_marker == "scriv:insert-here"
+
+
+def test_reading_config(temp_dir):
+    (temp_dir / ".scrivrc").write_text(CONFIG1)
+    config = read_config()
+    assert config.entry_directory == "changelog.d"
+    assert config.output_file == "README.md"
+    assert config.categories == ["New", "Different", "Gone", "Bad"]
+
+
+def test_reading_config_list(temp_dir):
+    (temp_dir / "tox.ini").write_text(CONFIG2)
+    config = read_config()
+    assert config.categories == ["New", "Different", "Gone", "Bad"]
