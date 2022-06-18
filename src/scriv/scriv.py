ac7e7ad3e6b37dbc9f23fa2278840323b0e27d8d
Ned Batchelder <ned@edx.org>
2022-06-18 07:14:19 -0400

    test: mypy wanted one more declaration

--- src/scriv/scriv.py
+++ src/scriv/scriv.py
@@ -193,7 +193,7 @@ def files_to_combine(config: Config) -> List[Path]:
     The paths are returned in the order they should be processed.
 
     """
-    paths = itertools.chain.from_iterable(
+    paths: Iterable[Path] = itertools.chain.from_iterable(
         [
             Path(config.fragment_directory).glob(pattern)
             for pattern in ["*.rst", "*.md"]
